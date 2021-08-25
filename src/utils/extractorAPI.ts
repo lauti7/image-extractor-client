92a8095444f0efd00a83013b41d7b0156c8ef4ea
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-25 20:20:47 -0300

    image-extractor-api functions

--- src/utils/extractorAPI.ts
+++ src/utils/extractorAPI.ts
@@ -1,2 +1,29 @@
-const API_URL = process.env.API_URL;
+import { ExtractResponse } from './interfaces';
 
+const API_URL = process.env.API_URL;
+const extractURL = `${API_URL}/extract/`;
+
+export const extractImages = (url: string): Promise<ExtractResponse> => {
+  return fetch(extractURL, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ weburl: url }),
+  })
+    .then((response) => response.json())
+    .then((json: ExtractResponse) => {
+      if (!json.error) {
+        return {
+          images: json.images,
+          error: false,
+        };
+      } else {
+        throw new Error('Server error');
+      }
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+};
