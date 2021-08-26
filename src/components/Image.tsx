18f29b938162424d906b8fb902e95e03ea1f6a5e
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-26 14:47:44 -0300

    fixed image size bug

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,22 +1,30 @@
-import React, { useRef } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { ImageResponse } from '../utils/interfaces';
 
 const Image = ({ image }: { image: ImageResponse }): JSX.Element => {
-  const imgRef = useRef<HTMLImageElement>(null);
+  const [size, setSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const handleImageLoad = (event: SyntheticEvent<HTMLImageElement>) => {
+    setSize({
+      width: event.currentTarget.naturalWidth,
+      height: event.currentTarget.naturalHeight,
+    });
+  };
 
   return (
     <div className="flex flex-col h-full bg-gray-800 rounded-md p-2">
       <div className="w-full h-3/4 rounded-md bg-white bg-opacity-10 flex flex-col justify-center items-center">
         <img
-          ref={imgRef}
+          onLoad={handleImageLoad}
           src={image.url}
           className="object-scale-down h-3/4 w-full"
         />
       </div>
       <div className="flex justify-between items-center mt-2">
-        <p className="px-1">
-          {`${imgRef?.current?.naturalWidth} x ${imgRef?.current?.naturalHeight}`}
-        </p>
+        <p className="px-1">{`${size.width} x ${size.height}`}</p>
         <p className="p-1 rounded bg-indigo-500 text-indigo-800">
           {image.type}
         </p>
