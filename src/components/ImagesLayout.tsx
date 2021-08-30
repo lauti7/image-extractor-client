713e1050071bd9b9c7afd8abb3031c539eee004e
Lautaro Bustos <lautigbustos@gmail.com>
2021-08-30 17:23:05 -0300

    server messages

--- src/components/ImagesLayout.tsx
+++ src/components/ImagesLayout.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import Image from './Image';
 import { ImageResponse } from '../utils/interfaces';
 
-const ImagesLayout = ({ images }: { images: ImageResponse[] }): JSX.Element => {
+const ImagesLayout = ({
+  images,
+  serverMessage,
+}: {
+  images: ImageResponse[];
+  serverMessage: string;
+}): JSX.Element => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-6 gap-2 auto-rows-fr mt-2">
-      {images.length > 0
-        ? images.map((img) => <Image key={img.fileName} image={img} />)
-        : null}
-    </div>
+    <>
+      {images.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-2 grid-flow-row auto-rows-fr mt-2">
+          {images.map((img) => (
+            <Image key={img.fileName} image={img} />
+          ))}
+        </div>
+      ) : (
+        <h3 className="mt-2 md:text-xl text-lg text-center">{serverMessage}</h3>
+      )}
+    </>
   );
 };
 
